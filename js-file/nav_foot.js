4a133b0634fc55ea262141b71dd19229d55d3829
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-15 19:43:38 +0530

    responsive first middle section

--- js-file/nav_foot.js
+++ js-file/nav_foot.js
@@ -1,7 +1,13 @@
 let bar =document.getElementById("hambar")
 let navbar = document.getElementById("navbar")
+let mid = document.getElementById("middle")
+console.log(bar)
+console.log(mid)
 if(bar){
     bar.addEventListener("click", ()=>{
         navbar.classList.toggle("active2");
     })
 }
+mid.addEventListener("click", ()=>{
+    navbar.classList.remove("active2")
+})
