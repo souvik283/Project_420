36878c6542c9fcfbb80a23bd7d99a032fe71e374
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-30 17:30:19 +0530

    change css and add js

--- js-file/cart.js
+++ js-file/cart.js
@@ -1,14 +1,10 @@
 let proceed_btn = document.getElementById("proceed")
-let tableinputs = document.getElementsByClassName("tbody")
-for (let i = 0; i < tableinputs.length; i++) {
-console.log(tableinputs[i].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML)
-}
-console.log(tableinputs[2].firstElementChild.nextElementSibling.nextElementSibling.nextElementSibling.innerHTML)
-proceed_btn.addEventListener("click",()=>{
-    let amount = document.getElementById("amount1");
-    let input=document.getElementById("input1")
-    let subtotal = document.getElementById("subtotal1")
-    console.log(amount.innerHTML* input.value)
-    subtotal.innerHTML=`$${(amount.innerHTML* input.value)}`
+let quantity = document.getElementsByClassName("quantity")
+let subtotal = document.getElementsByClassName("subtotal")
+let individual_amount = document.getElementsByClassName("amount")
 
+proceed_btn.addEventListener("click", () => {
+    for (let i = 0; i < individual_amount.length; i++) {
+    subtotal[i].innerHTML = `$${(individual_amount[i].innerHTML * quantity[i].value)}`
+    }
 })
