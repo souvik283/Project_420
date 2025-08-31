38ec9a472cd3f7cf5d9dc0575d1152b44179e803
Souvik Ghosh <ghosh2005202318@gmail.com>
2025-08-31 23:56:02 +0530

    change in ja

--- js-file/cart.js
+++ js-file/cart.js
@@ -5,13 +5,16 @@ let individual_amount = document.getElementsByClassName("amount")
 let cart_subtotal = document.getElementById("cart_subtotal")
 let shipping_fee = document.getElementById("shipping_fee")
 let total = document.getElementById("grand_total")
-let table1=document.getElementById("table1")
-let cart_total= document.getElementsByClassName("total")
-let total_para= cart_total[0].firstElementChild.firstElementChild.nextElementSibling.nextElementSibling;
-let amount = 0, shipping_amount, grand_total;
+let table1 = document.getElementById("table1")
+let cart_total = document.getElementsByClassName("total")
+let coupon = document.getElementById("coupon_code")
+let coupon_amount = document.getElementById("coupon_discount")
+let total_para = cart_total[0].firstElementChild.firstElementChild.nextElementSibling.nextElementSibling;
+let amount = 0, shipping_amount, grand_total, discount = 0;
 
 function amount_setting() {
-    amount=0
+    let ihtml = ``;
+    amount = 0
     for (let i = 0; i < individual_amount.length; i++) {
         amount += (individual_amount[i].innerHTML * quantity[i].value)
         subtotal[i].innerHTML = `$${(individual_amount[i].innerHTML * quantity[i].value)}`
@@ -19,20 +22,31 @@ function amount_setting() {
     // console.log(amount)
     cart_subtotal.innerHTML = `$${amount}`
     if (amount < 700) {
-    total_para.innerHTML=`* Add $${700-amount} more for free shipping`
+        ihtml = `* Add $${700 - amount} more for free shipping <br>`
         shipping_amount = 49;
         shipping_fee.innerHTML = `+ $${shipping_amount}`
     } else {
         shipping_amount = 0;
-        total_para.innerHTML=''
+        ihtml = ''
         shipping_fee.innerHTML = `Free`
     }
-    grand_total = amount + shipping_amount;
+    if (coupon.value == "souvik1111") {
+        ihtml += `** You got 69% discount`
+        discount = (amount * 69.0) / 100;
+        coupon_amount.innerHTML = `- $${discount}`
+    } else if (coupon.value != "" && coupon.value != "fuckyou69") {
+        // console.log(coupon.value)
+        ihtml += "** Enter a valid coupon code"
+        coupon.value = "";
+
+    }
+    total_para.innerHTML=ihtml
+    grand_total = amount + shipping_amount - discount;
     total.innerHTML = `$${grand_total}`
 
 }
 amount_setting()
 
-document.body.addEventListener("click", ()=>{
+document.body.addEventListener("click", () => {
     amount_setting()
 })
